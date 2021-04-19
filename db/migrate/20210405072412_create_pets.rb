3c2d3b145db2400287df83ca4ac46c47f3549a7d
YukiIshikawa1 <ishikawayuki0304@gmail.com>
2021-04-19 11:06:42 +0000

    fix ジャンルの削除

--- db/migrate/20210405072412_create_pets.rb
+++ db/migrate/20210405072412_create_pets.rb
@@ -2,7 +2,6 @@ class CreatePets < ActiveRecord::Migration[5.2]
   def change
     create_table :pets do |t|
       t.references :user, foreign_key: true
-      t.references :genre, foreign_key: true
       t.string :genre, null: false
       t.string :category, null: false
       t.string :name,    null: false
