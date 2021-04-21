50e3aeb7331f851f560d20952e8059c77bdeb00c
YukiIshikawa1 <ishikawayuki0304@gmail.com>
2021-04-21 17:16:51 +0000

    [fix レイアウト修正]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,13 +25,23 @@ $(function() {
   $('.slider').slick({
       centerMode: true, //スライド画面に次のスライドが表示される
       centerPadding: '10%', //次のスライドの幅
-      dots: true, //スライドの下にドットのナビゲーションを表示
       autoplay: true, //自動再生オン
       autoplaySpeed: 2000, //再生スピード
       infinite: true, //スライドが終了したら最初に戻る
       prevArrow: '<i class="fas fa-arrow-alt-circle-left"></i>',
       nextArrow: '<i class="fas fa-arrow-alt-circle-right"></i>',
-     fade:true
+      fade:true,
   });
 });
-
+$(function() {
+  $(".full").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    speed: 1500,
+    fade: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
+  });
+});
